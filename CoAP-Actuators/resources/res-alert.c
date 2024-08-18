0040e567a8142a553aafce43ce2c3dda4e0f23e5
RossSacco <rossanahh01@gmail.com>
2024-08-18 19:04:57 +0200

    res di regolazione temperatura

---
+++ CoAP-Actuators/resources/res-alert.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "contiki.h"
+#include "coap-engine.h"
+#include "os/dev/leds.h"
+#include "json_util/json.util.h"  //HO QUALCHE DUBBIO
+
+/* Log configuration */
+#include "sys/log.h"
+#define LOG_MODULE "App"
+#define LOG_LEVEL LOG_LEVEL_APP
